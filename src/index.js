4ee8d4afe76e144901f40e238014ac381f2c7eb3
Chris Kelley <kelleyc718@gmail.com>
2019-06-18 12:13:29 -0500

    nav changes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import AdminLayout from "layouts/Admin/Admin.jsx";
-import "assets/scss/black-dashboard-react.scss";
-import "assets/demo/demo.css";
-import "assets/css/nucleo-icons.css";
+import App from "./layouts/App";
+import "./assets/scss/black-dashboard-react.scss";
+import "./assets/demo/demo.css";
+import "./assets/css/nucleo-icons.css";
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
     <Router history={hist}>
         <Switch>
-            <Route path="/admin" render={props => <AdminLayout {...props} />} />
+            <Route path="/races" render={props => <App {...props} />} />
 
-            <Redirect from="/" to="/admin/dashboard" />
+            <Redirect from="/" to="/races/dashboard" />
         </Switch>
     </Router>,
     document.getElementById("root")
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,27 @@
-import Dashboard from "views/Dashboard.jsx";
-import Sydney from "views/Sydney.jsx";
-import SF from "views/SF.jsx";
+import Sydney from "./views/Sydney.jsx";
+import SF from "./views/SF.jsx";
 
 var routes = [
     {
         path: "/newyork",
         name: "New York",
-        icon: "tim-icons icon-chart-pie-36",
-        component: Dashboard,
-        layout: "/admin"
+        icon: "tim-icons icon-atom",
+        component: SF,
+        layout: "/races"
     },
     {
         path: "/sanfrancisco",
         name: "San Francisco",
         icon: "tim-icons icon-atom",
         component: SF,
-        layout: "/admin"
+        layout: "/races"
     },
     {
         path: "/sydney",
         name: "Sydney",
         icon: "tim-icons icon-pin",
         component: Sydney,
-        layout: "/admin"
+        layout: "/races"
     }
 ];
 export default routes;
