8acac677bf17804f37f0bd3bd6ce92e539760eac
Chris Kelley <kelleyc718@gmail.com>
2019-06-18 14:25:03 -0500

    fixed race fetching

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -26,7 +26,12 @@ import {
     UncontrolledTooltip
 } from "reactstrap";
 
-let chart1_2_options = {
+
+
+// #########################################
+// // // used inside src/views/Dashboard.jsx
+// #########################################
+const chartOptions = {
     maintainAspectRatio: false,
     legend: {
         display: false
@@ -76,83 +81,71 @@ let chart1_2_options = {
     }
 };
 
-// #########################################
-// // // used inside src/views/Dashboard.jsx
-// #########################################
+function raceData(id) {
+    console.log(id)
+    return
+}
 
 
+const chart = {
+    data1: canvas => {
+        let ctx = canvas.getContext("2d");
 
+        let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
 
-const raceData = async () => {
-    return await fetch("https://129.213.95.248:7002/ords/sailgp/series", { rejectUnauthorized: false })
-        .then(res => res.json())
-        .then(data => {
-            return ({
-                data1: canvas => {
-                    let ctx = canvas.getContext("2d");
+        gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+        gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+        gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
 
-                    let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-
-                    gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
-                    gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
-                    gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
-
-                    return {
-                        labels: [
-                            "JAN",
-                            "FEB",
-                            "MAR",
-                            "APR",
-                            "MAY",
-                            "JUN",
-                            "JUL",
-                            "AUG",
-                            "SEP",
-                            "OCT",
-                            "NOV",
-                            "DEC"
-                        ],
-                        datasets: [
-                            {
-                                label: "None",
-                                fill: true,
-                                backgroundColor: gradientStroke,
-                                borderColor: "#1f8ef1",
-                                borderWidth: 2,
-                                borderDash: [],
-                                borderDashOffset: 0.0,
-                                pointBackgroundColor: "#1f8ef1",
-                                pointBorderColor: "rgba(255,255,255,0)",
-                                pointHoverBackgroundColor: "#1f8ef1",
-                                pointBorderWidth: 20,
-                                pointHoverRadius: 4,
-                                pointHoverBorderWidth: 15,
-                                pointRadius: 4,
-                                data: [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100]
-                            }
-                        ]
-                    }
-                },
-                options: chart1_2_options
-            }
-            )
-        })
+        return {
+            labels: [
+                "JAN",
+                "FEB",
+                "MAR",
+                "APR",
+                "MAY",
+                "JUN",
+                "JUL",
+                "AUG",
+                "SEP",
+                "OCT",
+                "NOV",
+                "DEC"
+            ],
+            datasets: [
+                {
+                    label: "None",
+                    fill: true,
+                    backgroundColor: gradientStroke,
+                    borderColor: "#1f8ef1",
+                    borderWidth: 2,
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    pointBackgroundColor: "#1f8ef1",
+                    pointBorderColor: "rgba(255,255,255,0)",
+                    pointHoverBackgroundColor: "#1f8ef1",
+                    pointBorderWidth: 20,
+                    pointHoverRadius: 4,
+                    pointHoverBorderWidth: 15,
+                    pointRadius: 4,
+                    data: [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100]
+                }
+            ]
+        }
+    },
+    options: chartOptions
 }
 
 
 class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bigChartData: "",
-            races: [],
-            teams: [],
-            race: {
-                name: "Please Choose A Race",
-                id: ""
-            }
-        };
-    }
+    state = {
+        bigChartData: "",
+        races: [],
+        teams: [],
+        race: {
+            name: "Please Choose A Race"
+        }
+    };
 
     componentWillMount() {
         fetch(`https://129.213.95.248:7002/ords/sailgp/series`)
@@ -172,16 +165,10 @@ class Dashboard extends React.Component {
         });
     };
 
-    handleRaceClick = e => {
-        let race = e.target.value;
-        let id = e.target.key;
-        this.setState({
-            race: {
-                name: race,
-                id: id
-            }
-        })
-
+    fetchRace = (id) => {
+        fetch(`https://129.213.95.248:7002/ords/sailgp/raceinfo/stats/raceid/${id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
     }
 
     render() {
@@ -199,10 +186,17 @@ class Dashboard extends React.Component {
                                 >
                                     {this.state.race.name}
                                 </DropdownToggle>
-                                <DropdownMenu className="dropdown-navbar" tag="ul">
+                                <DropdownMenu className="dropdown">
                                     {this.state.races.map(race => {
                                         return (
-                                            <DropdownItem className="nav-item" key={race.id} value={race.name} onClick={(e) => this.handleRaceClick(e)}>
+                                            <DropdownItem key={race.id} value={race.id} onClick={
+                                                () => this.setState(
+                                                    {
+                                                        race: {
+                                                            name: race.name,
+                                                            id: race.id
+                                                        }
+                                                    }, this.fetchRace(race.id))} >
                                                 {race.name}
                                             </DropdownItem>
                                         )
@@ -211,46 +205,13 @@ class Dashboard extends React.Component {
                             </UncontrolledDropdown>
                         </Col>
                     </Row>
-
                     <Row>
                         <Col xs="12">
                             <Card className="card-chart">
                                 <CardHeader>
                                     <Row>
                                         <Col className="text-left" sm="6">
-                                            <h5 className="card-category">{this.state.race.name ? this.state.race.name : "Please Choose A Race"}</h5>
                                             <CardTitle tag="h2"></CardTitle>
-                                        </Col>
-                                        <Col sm="6">
-                                            <ButtonGroup
-                                                className="btn-group-toggle float-right"
-                                                data-toggle="buttons"
-                                            >
-                                                <Button
-                                                    tag="label"
-                                                    className={classNames("btn-simple", {
-                                                        active: this.state.bigChartData === "data1"
-                                                    })}
-                                                    color="info"
-                                                    id="0"
-                                                    size="sm"
-                                                    onClick={() => this.setBgChartData(raceData.data1)}
-                                                >
-                                                    <input
-                                                        defaultChecked
-                                                        className="d-none"
-                                                        name="options"
-                                                        type="radio"
-                                                    />
-                                                    <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
-                                                        Accounts
-                                                    </span>
-                                                    <span className="d-block d-sm-none">
-                                                        <i className="tim-icons icon-single-02" />
-                                                    </span>
-                                                </Button>
-
-                                            </ButtonGroup>
                                         </Col>
                                     </Row>
                                 </CardHeader>
